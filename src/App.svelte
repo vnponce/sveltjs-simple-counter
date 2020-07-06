ee4e866a75985b152d598e9e8d3f6d50b647b3f2
Abel Ponce <vnponce8@gmail.com>
2020-07-05 19:48:07 -0500

    first approach

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Counter from './components/Counter.svelte'
+	// export - Values that are passed in as props
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<Counter />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
---
+++ src/components/Counter.svelte
@@ -0,0 +1,31 @@
+<script>
+    export let inferior = 0;
+    export let superior = 9;
+    let counter = 0;
+
+    // the `$:` means 're-run whenever these values change'
+    // It's like a Vuejs computed property
+    $: isReduceDisable = counter <= inferior;
+    $: messageIsLessThan1 = isReduceDisable ? `No puede ser menor a ${counter}` : '';
+    $: isAddDisable = counter >= superior;
+    $: messageIsGreatherThan9 = isAddDisable ? `No puede ser mayor a ${counter}` : '';
+
+
+    function add() {
+      counter += 1;
+    }
+
+    function reduce() {
+      counter -= 1;
+    }
+</script>
+
+<p>Contador entre {inferior} y {superior}.</p>
+<button on:click={reduce} disabled={isReduceDisable}>-</button>
+    {counter}
+<button on:click={add} disabled={isAddDisable}>+</button>
+<div>
+    {messageIsLessThan1}
+    {messageIsGreatherThan9}
+</div>
+<hr>
